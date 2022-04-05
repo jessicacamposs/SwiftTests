7168255cde543cca6cb0529eeffdc346df10dad7
jessicacamposs <jessicasantanacampos@gmail.com>
2022-04-05 09:01:00 -0300

    First Commit

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -33,7 +33,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPurple
+        view.backgroundColor = .systemBrown
         view.addSubview(imageView)
         imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
         imageView.center = view.center
