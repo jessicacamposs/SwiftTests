afbb32a3e95c9cf537b9a9f7e38a0d433aa83518
jessicacamposs <jessicasantanacampos@gmail.com>
2022-04-05 08:53:24 -0300

    Initial Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Jessica Campos on 04/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private let imageView: UIImageView = {
+       let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.backgroundColor = .systemYellow
+        return imageView
+    }()
+    
+    private let button: UIButton = {
+       let button = UIButton()
+        button.backgroundColor = .black
+        button.setTitle("Random Photo", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
+    
+    let colors: [UIColor] = [
+        .systemPink,
+        .systemPurple,
+        .systemCyan,
+        .systemGray
+    ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPurple
+        view.addSubview(imageView)
+        imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
+        imageView.center = view.center
+        
+        view.addSubview(button)
+        getRandomPhoto()
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+    }
+    
+    @objc func didTapButton(){
+        getRandomPhoto()
+        
+        view.backgroundColor = colors.randomElement()
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        button.frame = CGRect (x: 30, y: view.frame.size.height-150-view.safeAreaInsets.bottom, width: view.frame.size.width-60, height: 55)
+    }
+    
+    func getRandomPhoto(){
+        let urlString = "https://source.unsplash.com/random/600x600"
+        let url = URL(string: urlString)!
+        guard let data = try? Data(contentsOf: url) else {
+            return
+        }
+        imageView.image = UIImage(data: data)
+    }
+
+}
+
